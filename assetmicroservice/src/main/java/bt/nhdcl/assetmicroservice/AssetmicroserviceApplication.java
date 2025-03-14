9967d71b94cee129e0862f91d5c59d3689519bb3
12210010.gcit <12210010.gcit@rub.edu.bt>
2025-03-14 10:42:02 +0600

    initial development

---
+++ assetmicroservice/src/main/java/bt/nhdcl/assetmicroservice/AssetmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package bt.nhdcl.assetmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetmicroserviceApplication.class, args);
+	}
+
+}
---
+++ assetmicroservice/src/test/java/bt/nhdcl/assetmicroservice/AssetmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.nhdcl.assetmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
